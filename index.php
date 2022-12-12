4ef0f6c0fcd7afbb2006142d32c4b91366d967be
Stanghi <stangherlin.matti@gmail.com>
2022-12-12 15:22:53 +0100

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- 
+- L’e-commerce vende **prodotti** per animali.
+- I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.  // TODO
+- I prodotti sono **cibo**, **giochi**, **cucce**, etc. // TODO
+
+StamPARE delle card contenenti i dettagli dei prodotti come:
+immagine // TODO
+titolo // TODO
+prezzo // TODO
+icona della categoria // TODO
+tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia) // TODO
+-->
+
+<?php
+
+require __DIR__ . '/Models/Category.php';
+require __DIR__ . '/Models/Product.php';
+require __DIR__ . '/Models/Food.php';
+require __DIR__ . '/Models/Toy.php';
+require __DIR__ . '/Models/Accessory.php';
